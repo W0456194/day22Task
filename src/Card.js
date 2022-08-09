583e335e7ca343d416bc7f41f74c98d9fa36dff1
W0456194 <82927744+W0456194@users.noreply.github.com>
2022-08-09 19:48:43 +0530

    firstcommit

---
+++ src/Card.js
@@ -0,0 +1,36 @@
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Card({item}) {
+    return (
+        
+        <div className="col-lg-4">
+        <div className="card mb-5 mb-lg-0">
+        <div className="card-body">
+            <h5 className="card-title text-muted text-uppercase text-center">{item.membership}</h5>
+            <h6 className="card-price text-center">${item.price}<span className="period">/month</span></h6>
+            <hr/>
+            <ul className="fa-ul">
+            {item.feature.map((list)=>{
+                return(
+            <li className={!list.isEnable ? "text-muted" : ""}>
+                <span className="fa-li">
+                    {/* <i className="fas fa-check"></i> */}
+                {list.isEnable ? <FontAwesomeIcon icon={faCheck}/> :<FontAwesomeIcon icon={faTimes} />}
+                    </span>
+                <span>
+                    {list.isBold ? <strong>{list.title}</strong> : list.title}
+                    </span>
+            </li>
+                )
+            })}
+            </ul>
+            <div className="d-grid">
+            <a href="#" className="btn btn-primary text-uppercase">Button</a>
+            </div>
+        </div>
+        </div>
+        </div>
+    );
+  }
+  export default Card;
